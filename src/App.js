0c6e25a5f9884c4a3fd1ea027cc933fce8660aa2
Alissa <alissa.vrk@gmail.com>
2022-05-09 10:49:34 +0300

    my changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import ReactDOM from 'react-dom';
 
-function getLogo() {
-  if (window.chrome) {
-    return window.chrome.runtime.getURL(logo);
-  }
+const CounterContext = React.createContext(1);
 
-  return logo;
+function CounterProvider({children}) {
+  const [count, setCount] = useState(1);
+  const value = useMemo(() => ({count, setCount}), [count]);
+
+return (<CounterContext.Provider value={value}>
+    {children}
+  </CounterContext.Provider>
+)}
+
+function PortalledComponent({container}) {
+  const {count} = useContext(CounterContext);
+  return ReactDOM.createPortal(
+    <div>REACT COUNT {count}</div>,
+    container
+  );
 }
 
+function CounterComp(){
+  const {setCount, count} = useContext(CounterContext);
+
+  return <button className='my-button' onClick={() => setCount(count + 1)}>CLICK COUNTER BUTTON</button>
+}
+
+
+
 function App() {
+  const [divs, setDiv] = useState([]);
+  useEffect(() => {
+    const divs = document.getElementsByTagName('div');
+    setDiv(Array.from(divs));
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={getLogo()} className="App-logo" alt="logo" />
-        <p>Hello, World!</p>
-        <p>I'm a Content Script in a Chrome Extension!</p>
-      </header>
+      <CounterProvider>
+        <CounterComp />
+     {divs.map(div => <PortalledComponent container={div}/>)}
+     </CounterProvider>
     </div>
   );
 }
